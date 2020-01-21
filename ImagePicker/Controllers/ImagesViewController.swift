ad54d93b93c6be435f76a79430c6f3c112426910
LubaKaper <liubovkaper@pursuit.com>
2020-01-21 15:55:28 -0500

    image picker

--- ImagePicker/Controllers/ImagesViewController.swift
+++ ImagePicker/Controllers/ImagesViewController.swift
@@ -27,10 +27,10 @@ import UIKit
 import AVFoundation // we want to use AVMakeRect(0 to maintain image aspect ratio
 
 class ImagesViewController: UIViewController {
-  
-  @IBOutlet weak var collectionView: UICollectionView!
-      
-  private var imageObjects = [ImageObject]()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var imageObjects = [ImageObject]()
     
     private var imagePickerController = UIImagePickerController()
     
@@ -39,21 +39,21 @@ class ImagesViewController: UIViewController {
     private var selectedImage: UIImage? {
         didSet {
             // gets called when new image is selected
-           // collectionView.reloadData()
+            // collectionView.reloadData()
             appendNewPhotoToCollection()
         }
     }
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    collectionView.dataSource = self
-    collectionView.delegate = self
-    
-    // set UIImagePickerCintri=oller delegate as this viewcontroller
-    imagePickerController.delegate = self
-    
-    loadImageObjects()
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self 
+        collectionView.delegate = self
+        
+        // set UIImagePickerCintri=oller delegate as this viewcontroller
+        imagePickerController.delegate = self
+        
+        loadImageObjects()
+    }
     // function to load saved data
     private func loadImageObjects() {
         do {
@@ -62,13 +62,13 @@ class ImagesViewController: UIViewController {
             print("loading objects error \(error)")
         }
     }
-   
+    
     private func appendNewPhotoToCollection() {
         guard let image = selectedImage
             //jpegData(compressionQuality: 1.0) converts UIImage to data
-else {
-            print("image is nil")
-            return
+            else {
+                print("image is nil")
+                return
         }
         
         print("original image size is \(image.size)")
@@ -103,14 +103,14 @@ else {
         
         // persist imageObject to documents directory
         do {
-        try dataPersistance.create(item: imageObject)
+            try dataPersistance.create(item: imageObject)
         } catch {
             print("saving error: \(error)")
         }
     }
     
     @IBAction func addPictureButtonPressed(_ sender: UIBarButtonItem) {
-      // present an action sheet to the user
+        // present an action sheet to the user
         // actions: camera, photo library, cancel
         // preferredStyle: .alert(will show actions in the middle of the screen)
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -127,7 +127,7 @@ else {
         // check if camaera is available. If camera is not available and you attampt to show the camera the app will crash
         
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-        alertController.addAction(cameraAction)
+            alertController.addAction(cameraAction)
         }
         alertController.addAction(photoLibrary)
         alertController.addAction(cancelAction)
@@ -148,32 +148,32 @@ else {
 
 // MARK: - UICollectionViewDataSource
 extension ImagesViewController: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return imageObjects.count
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    
-   // step5: creating custom delegation: - must have an instance of object B
-    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as? ImageCell else {
-      fatalError("could not downcast to an ImageCell")
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageObjects.count
     }
-    let imageObject = imageObjects[indexPath.row]
-    cell.configureCell(imageObject: imageObject)
-    // step5: creating custom delegation: - set delegate object
-    cell.delegate = self
-    return cell
-  }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // step5: creating custom delegation: - must have an instance of object B
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as? ImageCell else {
+            fatalError("could not downcast to an ImageCell")
+        }
+        let imageObject = imageObjects[indexPath.row]
+        cell.configureCell(imageObject: imageObject)
+        // step5: creating custom delegation: - set delegate object
+        cell.delegate = self
+        return cell
+    }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension ImagesViewController: UICollectionViewDelegateFlowLayout {
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    // width of the device
-    let maxWidth: CGFloat = UIScreen.main.bounds.size.width
-    // cell width is 80% of device width
-    let itemWidth: CGFloat = maxWidth * 0.80
-    return CGSize(width: itemWidth, height: itemWidth)  }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // width of the device
+        let maxWidth: CGFloat = UIScreen.main.bounds.size.width
+        // cell width is 80% of device width
+        let itemWidth: CGFloat = maxWidth * 0.80
+        return CGSize(width: itemWidth, height: itemWidth)  }
 }
 
 extension ImagesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -201,7 +201,7 @@ extension ImagesViewController: ImageCellDelegate {
         guard let indexPath = collectionView.indexPath(for: imageCell) else {
             return
         }
-    // present an action sheet
+        // present an action sheet
         // actions: delete, cancel
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { [weak self] alertAction in
@@ -212,7 +212,7 @@ extension ImagesViewController: ImageCellDelegate {
         alertController.addAction(deleteAction)
         alertController.addAction(cancelAction)
         present(alertController, animated: true)
-      //  print(indexPath.row)
+        //  print(indexPath.row)
     }
     
     private func deleteImageObject(indexPath: IndexPath) {
@@ -224,7 +224,7 @@ extension ImagesViewController: ImageCellDelegate {
             // delete image object from imageObjects
             imageObjects.remove(at: indexPath.row)
             
-                collectionView.deleteItems(at: [indexPath])
+            collectionView.deleteItems(at: [indexPath])
             
         } catch {
             print("error deleting item \(error)")
@@ -235,12 +235,12 @@ extension ImagesViewController: ImageCellDelegate {
 // more here: https://nshipster.com/image-resizing/
 // MARK: - UIImage extension
 extension UIImage {
-  func resizeImage(to width: CGFloat, height: CGFloat) -> UIImage {
-    let size = CGSize(width: width, height: height)
-    let renderer = UIGraphicsImageRenderer(size: size)
-    return renderer.image { (context) in
-      self.draw(in: CGRect(origin: .zero, size: size))
+    func resizeImage(to width: CGFloat, height: CGFloat) -> UIImage {
+        let size = CGSize(width: width, height: height)
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { (context) in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
     }
-  }
 }
 
